f37f4fc4a0d70e3094a7353035bfd5f1bc512ee0
Andre Copetti <andrecopetti@gmail.com>
2024-01-07 14:13:32 -0300

    grabbing elements from HTML amazon website and adding the data to an Array

--- index.js
+++ index.js
@@ -3,13 +3,56 @@ import puppeteer from 'puppeteer';
 (async () => {
 
     const browser = await puppeteer.launch({
-        headless : false
+        headless : false,
+        defaultViewport: false,
+        userDataDir: "./tmp"
     });
+
     const page = await browser.newPage();
 
-    await page.goto('https://example.com');
+    await page.goto('https://www.amazon.com.br/gp/bestsellers/books');
 
-    await page.screenshot({path : 'example.png'})
+    const productHandles = await page.$$(
+        '.a-cardui'
+    );
+
+    let i = 0;
+
+    let items = [];
+
+    for(const productHandle of productHandles){
+
+        let title = "Null";
+        let price = "Null";
+        let image = "Null";
+
+        try {
+            title = await page.evaluate(
+                el => el.querySelector('a > span > div').textContent,
+                productHandle
+            );
+        }catch (error){}
+
+        try {
+            price = await page.evaluate(
+                el => el.querySelector('.a-color-price > span').textContent,
+                productHandle
+            );
+        }catch (error){}
+
+        try {
+            image = await page.evaluate(
+                el => el.querySelector('.a-dynamic-image').src,
+                productHandle
+            );
+        }catch (error){}
+
+        if(title !== "Null"){
+            items.push({title, price, image});
+        }
+    }
 
     await browser.close();
+
+    console.log(items);
 })();
